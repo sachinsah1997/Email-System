445501598bda620cd12ee79947077835dcc03ce9
Sachin Sah <ssk1222014@gmail.com>
2020-12-28 20:13:45 +0530

    Spash Screen Added

---
+++ app/src/main/java/com/sachinsah/emailsystem/SplashScreenActivity.java
@@ -0,0 +1,44 @@
+package com.sachinsah.emailsystem;
+
+import android.app.ActionBar;
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashScreenActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.requestWindowFeature(Window.FEATURE_ACTION_BAR);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_splash_screen);
+
+        Thread timer = new Thread(){
+            public void run(){
+                try{
+                    sleep(1000);
+                }
+                catch(InterruptedException e){
+                    e.printStackTrace();
+                } finally {
+                    Intent openMain = new Intent(SplashScreenActivity.this, GmailAuthActivity.class);
+                    startActivity(openMain);
+                    finish();
+                }
+            }
+        };
+        timer.start();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        finish();
+    }
+}
